23e0fdccfcec414c490277c0d3288491850bbe6a
LeilaAfhim <leilaafhim@gmail.com>
2020-12-24 21:17:36 +0100

    forecast created

--- script.js
+++ script.js
@@ -48,12 +48,58 @@ function getCurrentLocation(event){
 
 //challenge 2 challenge api
 
+function formatHours(timestamp){
+    let date= new Date(timestamp);
+    let hours= date.getHours();
+    if (hours<10){
+        hours=`0${hours}`;
+    }
+let minutes= date.getMinutes();
+if(minutes<10){
+    minutes=`0${minutes}`;
+}
+return `${hours}:${minutes}`;
+
+}
+
+
+function displayForecast(response){
+let forecastElement = document.querySelector("#forecast");
+forecastElement.innerHTML= null;
+let forecast= null;
+
+for(let index=0; index < 5 ;index ++){
+forecast= response.data.list[index];
+
+    forecastElement.innerHTML +=
+ `<button class="next-days">
+        ${formatHours(forecast.dt*1000)}
+            <br />
+            <h3>${Math.round(forecast.main.temp)}°C
+                <br />
+                <img src="https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png">
+            </h3>
+        </button>`;
+}
+
+}
+
+
+
 function searchCity(city){
+
+
+   
     let units ="metric";
     let apiKey = "62d7633f840fb3f4bca3fffc5afee380";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 
+
 axios.get(apiUrl).then(showTemperature);
+
+apiUrl= `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=${units}`;
+axios.get(apiUrl).then(displayForecast);
+
 }
 
 
@@ -84,9 +130,10 @@ function showFahrenheit (event){
     let fahrenheitTemp = (celciusTemperature*9)/5+32;
     let temperatureElement=document.querySelector("#temperature");
     temperatureElement.innerHTML = Math.round(fahrenheitTemp);
+
+
 celcius.classList.remove("active");
     fahrenheit.classList.add("active");
-
 }
 
 function showCelcius (event){
